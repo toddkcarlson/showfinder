18a9d6371f31c77021c25b07563b78c45440c689
Todd Carlson <tcarlson@carlsonsw.com>
2020-03-23 22:28:26 -0400

    removed active record

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,17 @@
 require_relative 'boot'
 
-require 'rails/all'
+# require 'rails/all'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'active_model/railtie'
+require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
+require 'action_cable/engine'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
 
 module Showfinder
   class Application < Rails::Application
