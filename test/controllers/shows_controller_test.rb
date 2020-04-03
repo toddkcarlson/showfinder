417976c39014f9b20eca19d3d0710deb31f819e8
Todd Carlson <tcarlson@carlsonsw.com>
2020-04-03 09:12:03 -0400

    misc

--- test/controllers/shows_controller_test.rb
+++ test/controllers/shows_controller_test.rb
@@ -14,29 +14,34 @@ end
 
 class ShowsControllerTest < ActionDispatch::IntegrationTest
 
-       got = {
+       @@got = {
             "original_name" => "Game of Thrones",
             "vote_average" => 10,
-            "poster_path" => "path/to/some_image.jpg"
+            "poster_path" => "path/to/some_image.jpg",
+            "backdrop_path" => "path/to/some_image.jpg",            
+            "overview" => "A TV series."            
         }
 
-        ad = {
+        @@ad = {
             "original_name" => "Arrested Development",
-            "vote_average" => 10
+            "vote_average" => 10,
+            "poster_path" => "path/to/some_image.jpg",
+            "backdrop_path" => "path/to/some_image.jpg",            
+            "overview" => "A TV series."               
         }
 
-        results = [got, ad]
+        @@results = [@@got, @@ad]
 
     def test_main_page
 
         mock_tv_api({
             path: '/trending/tv/week',
-            body: {"results" => results}})
+            body: {"@@results" => @@results}})
 
       get "/"
       assert_response :success
 
-      results.each do |r|
+      @@results.each do |r|
           # Names of the shows should appear:
           assert_match r["original_name"], response.body
 
@@ -45,10 +50,10 @@ class ShowsControllerTest < ActionDispatch::IntegrationTest
       end
 
       # Rating of the show appear:
-      assert_match "User Rating: " + got["vote_average"], response.body
+      assert_match "User Rating: #{@@got["vote_average"]}", response.body
 
       # Poster url should appear:
-      assert_match got["poster_path"], response.body
+      assert_match @@got["poster_path"], response.body
 
       # Default image should appear for show without poster_path:
       assert_match 'class="poster_default"', response.body
@@ -60,12 +65,12 @@ class ShowsControllerTest < ActionDispatch::IntegrationTest
         mock_tv_api({
             path: '/search/tv',
             qstring: "&query=" + search,
-            body: {"results" => results}})
+            body: {"@@results" => @@results}})
 
       get "/?search=" + search
       assert_response :success
 
-      results.each do |r|
+      @@results.each do |r|
           # Names of the shows should appear:
           assert_match r["original_name"], response.body
 
@@ -74,10 +79,10 @@ class ShowsControllerTest < ActionDispatch::IntegrationTest
       end
 
       # Rating of the show appear:
-      assert_match "User Rating: " + got["vote_average"], response.body
+      assert_match "User Rating: #{@@got["vote_average"]}", response.body
 
       # Poster url should appear:
-      assert_match got["poster_path"], response.body
+      assert_match @@got["poster_path"], response.body
 
       # Default image should appear for show without poster_path:
       assert_match 'class="poster_default"', response.body
@@ -85,20 +90,20 @@ class ShowsControllerTest < ActionDispatch::IntegrationTest
 
     def test_show_page
 
-      get "/shows?data=" + CGI::escape(got.to_json)
+      get "/shows?data=" + CGI::escape(@@got.to_json)
       assert_response :success
 
       # Name should appear:
-      assert_match got["original_name"], response.body
+      assert_match @@got["original_name"], response.body
 
       # Rating of the show appear:
-      assert_match "User Rating: " + got["vote_average"], response.body
+      assert_match "User Rating: #{@@got["vote_average"]}", response.body
 
       # Poster url should appear:
-      assert_match got["poster_path"], response.body
+      assert_match @@got["backdrop_path"], response.body
 
       # Description should appear:
-      assert_match got["overview"], response.body
+      assert_match @@got["overview"], response.body
 
     end    
 end
